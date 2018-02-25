11b2a2d7dba1c669089c595b757606637b514b1c
Joshua Simpson <contact@josh-simpson.me>
2018-02-25 10:39:56 +0000

    Trying to get file upload

--- server.py
+++ server.py
@@ -1,28 +1,55 @@
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
 from secrets import CLARIFAI_KEY
+from werkzeug.utils import secure_filename
+from flask import Flask, request, redirect, url_for
 
-from flask import Flask
 import pymongo
 import json
+import os
 
+UPLOAD_FOLDER = '/uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def hello():
-    clarifai = ClarifaiApp(api_key=CLARIFAI_KEY)
-    model = clarifai.models.get('color')
-    response = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-    if 'Red' not in response:
-        return json.dumps({'inventory_status': 'poor'})
-    else:
-        return json.dumps({'inventory_status': 'ok'})
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
